531ebbd0c9a03ff67f2e7e95583ff0a083994aa8
mango <esudatcom@gmail.com>
2024-03-11 11:41:46 +0900

    HTTP Request의 내용을 한 줄씩 출력 while/logger

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -24,7 +24,15 @@ public class RequestHandler implements Runnable {
         // InputStream을 BufferedReader 로 바꿔주는 API를 확인한다?
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            System.out.println("request : " + br.readLine());
+            String line = br.readLine(); // 첫 번재 라인에 해당하는 이 부분에서 index.html을 읽은 다음에 추출 -> src/main/resources/static 디렉토리에 있는 Hello World 대신 응답으로 쏴주면
+            logger.debug("request line : {}", line);
+            System.out.println("request : " + line); // 한 줄만 출력됨
+            // 요청에 대한 모든 라인(데이터)을 찍어보려면? while 문 돌면서 찍어본다
+            while (!line.equals("")){ // 공백문자열을 만나기 전 까지 , 공백문자열 만나면 while 문 빠져나감
+                line = br.readLine();
+                logger.debug("header : {}", line);
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "<h1>Hello World</h1>".getBytes();
