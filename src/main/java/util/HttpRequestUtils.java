b5f472961dee0b2354d936f5c41a354b4889f8fb
mango <esudatcom@gmail.com>
2024-03-12 11:16:20 +0900

    모든 Request Header 출력되게 구현

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -7,11 +7,16 @@ import webserver.RequestHandler;
 public class HttpRequestUtils {
     private static final Logger logger = LoggerFactory.getLogger(HttpRequestUtils.class); // logger 는 로그 메시지를 출력하기 위해 사용된다.
 
-    public static String getUrl(String firstLine){
+    public static String getUrl(String firstLine) {
         // 첫 번째 라인에서 요청 URL 추출, 스페이스 기반으로 스플릿
         String[] tokens = firstLine.split(" ");
-        String path = tokens[1];
-        logger.debug("request path : {}", path);
-        return path;
+        if (tokens.length > 1) {
+            String path = tokens[1];
+            logger.debug("request path : {}", path);
+            return path;
+        } else {
+            logger.error("Invalid Request Line: {}", firstLine);
+            return "";
+        }
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,8 +10,8 @@ import util.HttpRequestUtils;
 
 // 이 서버는 클라이언트의 요청을 받아 처리하고,
 // index.html 파일의 내용을 클라이언트에게 응답으로 보내는 역할을 한다.
-public class RequestHandler implements Runnable { // ❓Runnable 인터페이스를 구현하여, 쓰레드에서 실행될 수 있도록 한다.
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class); // logger 는 로그 메시지를 출력하기 위해 사용된다.
+public class RequestHandler implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class); // 로그 메시지를 출력하기 위해 사용된다.
 
     private Socket connection; // 클라이언트와의 네트워크 연결을 나타낸다.
 
@@ -24,14 +24,24 @@ public class RequestHandler implements Runnable { // ❓Runnable 인터페이스
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
         // 입출력 스트림 준비 : 클라이언트와의 데이터 교환을 위해 입력 스트림과 출력 스트림을 준비한다.
+        // 브라우저에서 서버쪽으로 들어오는 모든 데이터는 InputStream 에 담겨있음
+        // 서버에서 브라우저로의 응답은 OutStream 에 실어서 보내다.
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // 클라이언트로부터 HTTP 요청을 한 줄씩 읽어들인다.
-            // 첫 번째 줄은 요청 라인을 포함하고, 그 다음 줄들은 요청 헤더를 포함한다.
             // 일반적으로 헤더는 라인 단위로 구성된다. 라인 단위로 데이터를 읽기 위해 BufferedReader 로 변경
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine(); // 첫 번째 라인에 해당하는 이 부분에서 index.html을 읽은 다음에 추출 -> src/main/resources/static 디렉토리에 있는 Hello World 대신 응답으로 쏴주면
+
             if(line == null){ // null 처리, null인 경우 무시한다.
                 return;
+            }
+
+            // 첫 번째 요청 라인 로깅
+            logger.debug("Request Line : {}", line);
+
+            // 헤더 라인 읽고 출력 -> 빈 라인이 나타날때 까지 반복 -> 모든 Request Header 출력
+            while (!line.equals("")){
+                line = br.readLine();
+                logger.debug("Header : {}", line);
             }
 
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
@@ -54,8 +64,6 @@ public class RequestHandler implements Runnable { // ❓Runnable 인터페이스
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            // 텍스트만 잘 나오게 처리가 되어 있음 -> img 도 나오게 처리 해줘야함
-            // local host만 띄웠을 때, dafault로 index.html이 뜨게끔 처리를 해줘야 한다
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
 //            dos.writeBytes("Content-Type: " + getContentType(fileName) + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
