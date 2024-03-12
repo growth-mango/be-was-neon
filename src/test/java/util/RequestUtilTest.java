feda8a727cff6c5e14f7d2b85a99e8bf19652c15
mango <esudatcom@gmail.com>
2024-03-12 16:01:29 +0900

    readme 작성

--- src/test/java/util/RequestUtilTest.java
+++ src/test/java/util/RequestUtilTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class RequestUtilTest {
 
     @Test
-    @DisplayName("/index.html 이 잘 추출된다")
+    @DisplayName("/index.html 이 추출되면 테스트가 성공한다")
     public void testGetUrl(){
         // 실제값 , 기대값
         assertThat("/index.html").isEqualTo(RequestUtil.getUrl("GET /index.html HTTP1.1"));
