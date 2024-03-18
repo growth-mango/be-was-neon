2286c70aa14dc3e105786c7caeb4b83d89838bee
mango <esudatcom@gmail.com>
2024-03-18 19:48:08 +0900

    리팩토링 : run 메소드 분리

--- src/main/java/httpMessage/HttpRequest.java
+++ src/main/java/httpMessage/HttpRequest.java
@@ -1,7 +1,13 @@
 package httpMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.RequestLineParser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,10 +16,17 @@ public class HttpRequest {
     private String requestLine;
     private Map<String, String> headers = new HashMap<>();
     private Map<String, String> bodies = new HashMap<>();
+    private Map<String, String> queries = new HashMap<>();
+    private String requestURL;
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
 
-    public HttpRequest(BufferedReader br) throws IOException {
+    public HttpRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
         this.requestLine = br.readLine(); // 요청 라인 읽기
         readHeaders(br); // 헤더 읽기
+        RequestLineParser requestLineParser = new RequestLineParser(this.requestLine);
+        this.requestURL = requestLineParser.getRequestURL(); // 요청 URL 저장
+        this.queries = requestLineParser.getQueries(); // 쿼리 파라미터 저장
     }
 
     private void readHeaders(BufferedReader br) throws IOException {
@@ -28,6 +41,12 @@ public class HttpRequest {
         }
     }
 
+    public void printHeaders(Map<String, String> headers) {
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            logger.debug("Header Key: \"{}\" Value: \"{}\"", header.getKey(), header.getValue());
+        }
+    }
+
     public String getRequestLine() {
         return requestLine;
     }
@@ -38,5 +57,17 @@ public class HttpRequest {
 
     public Map<String, String> getBodies() {
         return bodies;
+    }
+
+    public Map<String, String> getQueries() {
+        return queries;
+    }
+
+    public String getRequestURL() {
+        return requestURL;
+    }
+
+    public String getValue(String key){
+        return getQueries().get(key);
     }
 }
