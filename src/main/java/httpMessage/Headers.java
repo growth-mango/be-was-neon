bfc4c67fe9d8eb397d89f9a270abb874453f3498
mango <esudatcom@gmail.com>
2024-03-20 17:59:49 +0900

    리팩토링 : HttpRequest 클래스 정리

--- src/main/java/httpMessage/Headers.java
+++ src/main/java/httpMessage/Headers.java
@@ -1,5 +1,7 @@
 package httpMessage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +23,18 @@ public class Headers {
     public int getContentLength() {
         String value = headers.get("Content-Length");
         return value != null ? Integer.parseInt(value) : 0;
+    }
+
+    public void readHeaders(BufferedReader br) throws IOException {
+        String line ;
+        while ((line = br.readLine()) != null && !line.isEmpty()) { // 첫 번째 라인 (요청 라인) 은, 헤더가 아니기에 건너뛰고 시작한다.
+            int separator = line.indexOf(":");
+            if (separator != -1) {
+                String name = line.substring(0, separator).trim();
+                String value = line.substring(separator + 1).trim();
+                add(name, value);
+            }
+        }
     }
 
     public void printHeaders() {
